a2387f65beaae0bdd6c2f8085b72e44f6c755de1
Chris Fifty <cjf92@cornell.edu>
2018-12-02 16:47:01 -0500

    cleanup tensorflow code so it just contains GCN related work

--- tfGCN/train.py
+++ tfGCN/train.py
@@ -5,7 +5,7 @@ import time
 import tensorflow as tf
 
 from utils import *
-from models import GCN, HashedGCN, MLP
+from models import GCN, HashedGCN
 
 # Set random seed
 seed = 123
@@ -16,14 +16,13 @@ tf.set_random_seed(seed)
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_string('dataset', 'cora', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed'
-flags.DEFINE_string('model', 'gcn', 'Model string.')  # 'gcn', 'gcn_cheby', 'hashed_gcn', 'dense'
+flags.DEFINE_string('model', 'gcn', 'Model string.')  # 'gcn','hashed_gcn'
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
 flags.DEFINE_integer('epochs', 200, 'Number of epochs to train.')
 flags.DEFINE_integer('hidden1', 16, 'Number of units in hidden layer 1.')
 flags.DEFINE_float('dropout', 0.5, 'Dropout rate (1 - keep probability).')
 flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
 flags.DEFINE_integer('early_stopping', 10, 'Tolerance for early stopping (# of epochs).')
-flags.DEFINE_integer('max_degree', 3, 'Maximum Chebyshev polynomial degree.')
 flags.DEFINE_integer('compression_factor', 2, 'Hashed GCN weight compression factor.')
 
 # Load data
@@ -35,18 +34,10 @@ if FLAGS.model == 'gcn':
     support = [preprocess_adj(adj)]
     num_supports = 1
     model_func = GCN
-elif FLAGS.model == 'gcn_cheby':
-    support = chebyshev_polynomials(adj, FLAGS.max_degree)
-    num_supports = 1 + FLAGS.max_degree
-    model_func = GCN
 elif FLAGS.model == 'hashed_gcn':
     support = [preprocess_adj(adj)]
     num_supports = 1
     model_func = HashedGCN
-elif FLAGS.model == 'dense':
-    support = [preprocess_adj(adj)]  # Not used
-    num_supports = 1
-    model_func = MLP
 else:
     raise ValueError('Invalid argument for model: ' + str(FLAGS.model))
 
