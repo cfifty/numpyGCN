6061a313a95113d8969ea4a0b960c4419ae099ce
Danny Qiu <dqiu55@gmail.com>
2018-12-02 17:42:29 -0500

    Refactor dropout and weight decay into model

--- numpyGCN/numpyGCN.py
+++ numpyGCN/numpyGCN.py
@@ -6,7 +6,7 @@ from utils import softmax, softmax_cross_entropy_deriv, relu, relu_diff
 class numpyGCN:
 
     # two layer GCN
-    def __init__(self, input_dim, hidden_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout=None, weight_decay=0):
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
@@ -15,27 +15,26 @@ class numpyGCN:
         self.in_2 = None
         self.out_2 = None
 
+        self.dropout = dropout
+        self.weight_decay = weight_decay
+
         # randomly initialize weight matrices according to Glorot & Bengio (2010)
         self.W_1 = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (input_dim, hidden_dim))
         self.W_2 = np.random.uniform(-np.sqrt(1./output_dim), np.sqrt(1./output_dim), (hidden_dim, output_dim))
 
     # simple forward pass
-    def forward(self, X, A, drop=None):
-        if drop:
-            d1, d2 = drop
-        
-        if drop:
-            self.in_1 = A.dot(X).dot(self.W_1 * d1)
-        else:
-            self.in_1 = A.dot(X).dot(self.W_1)
-        
+    def forward(self, X, A, drop_weights=None):
+        W_1 = self.W_1
+        W_2 = self.W_2
+
+        if drop_weights:
+            d1, d2 = drop_weights
+            W_1 = W_1 * d1
+            W_2 = W_2 * d2
+
+        self.in_1 = A.dot(X).dot(W_1)
         self.out_1 = relu(self.in_1)
-        
-        if drop:
-            self.in_2 = A.dot(self.out_1).dot(self.W_2 * d2)
-        else:
-            self.in_2 = A.dot(self.out_1).dot(self.W_2)
-        
+        self.in_2 = A.dot(self.out_1).dot(W_2)
         self.out_2 = softmax(self.in_2)
         return self.out_2
 
@@ -53,30 +52,30 @@ class numpyGCN:
         return num_correct / expected_class.shape[0]
 
     # normalized cross entropy loss
-    def calc_loss(self, X, Y, A, mask, weight_decay=0):
+    def calc_loss(self, X, Y, A, mask):
         N = mask.sum()
         preds = self.forward(X, A)
         loss = np.sum(Y[mask] * np.log(preds[mask]))
         loss = np.asscalar(-loss) / N
 
-        if weight_decay:
-            l2_reg = np.sum(np.square(self.W_1)) * weight_decay/2
-            return loss + l2_reg
+        if self.weight_decay:
+            l2_reg = np.sum(np.square(self.W_1)) * self.weight_decay / 2
+            loss = loss + l2_reg
 
-        return loss 
+        return loss
 
     # back propagation
-    def backprop(self, X, Y, A, mask, d, w_d=0):
+    def backprop(self, X, Y, A, mask):
         dW_1 = np.zeros(self.W_1.shape)
         dW_2 = np.zeros(self.W_2.shape)
 
         # divide by d so expectation of GCN layer doesn't change from train to test
-        if d:
-            d1 = np.random.binomial(1, (1-d), size=self.W_1.shape) / (1-d)
-            d2 = np.random.binomial(1, (1-d), size=self.W_2.shape) / (1-d)
+        if self.dropout:
+            d1 = np.random.binomial(1, (1-self.dropout), size=self.W_1.shape) / (1-self.dropout)
+            d2 = np.random.binomial(1, (1-self.dropout), size=self.W_2.shape) / (1-self.dropout)
             preds = self.forward(X, A, (d1,d2))
-        else: 
-            preds = self.forward(X,A)
+        else:
+            preds = self.forward(X, A)
 
         # IMPORTANT: update gradient based only on masked labels
         preds[~mask] = Y[~mask]
@@ -96,17 +95,17 @@ class numpyGCN:
 
         dL_dW1 = dIn1_dW1.dot(dL_dIn1)
 
-        if w_d:
-            dL_dW1 += w_d * self.W_1
+        if self.weight_decay:
+            dL_dW1 += self.weight_decay * self.W_1
 
-        if d:
-            dL_dW1 *= d1 
+        if self.dropout:
+            dL_dW1 *= d1
             dL_dW2 *= d2
         return (dL_dW1, dL_dW2)
 
-    def gd_update(self, X, Y, A, mask, lr, d, w_d):
+    def gd_update(self, X, Y, A, mask, lr):
         # compute weight gradients
-        dW_1, dW_2 = self.backprop(X, Y, A, mask, d, w_d)
+        dW_1, dW_2 = self.backprop(X, Y, A, mask)
 
         # TODO: drop loss by set amount during training...
         #loss = self.calc_loss(X, Y, A, mask)
--- numpyGCN/train.py
+++ numpyGCN/train.py
@@ -7,16 +7,16 @@ import numpy as np
 from numpyGCN import numpyGCN
 from utils import load_data
 
-def train_with_gd(model, features, adj, y_train, y_val, train_mask, val_mask, early_stopping, lr, d, w_d, epochs):
+def train_with_gd(model, features, adj, y_train, y_val, train_mask, val_mask, early_stopping, lr, epochs):
     t_total = time.time()
     best_val_loss, val_epoch = float('inf'), 0
     for epoch in range(epochs):
         start = time.time()
-        model.gd_update(features, y_train, adj, train_mask, lr, d, w_d)
+        model.gd_update(features, y_train, adj, train_mask, lr)
         end = time.time()
 
-        train_loss = model.calc_loss(features, y_train, adj, train_mask, w_d)
-        val_loss = model.calc_loss(features, y_val, adj, val_mask, w_d)
+        train_loss = model.calc_loss(features, y_train, adj, train_mask)
+        val_loss = model.calc_loss(features, y_val, adj, val_mask)
 
         train_accuracy = model.compute_accuracy(features, y_train, adj, train_mask)
         val_accuracy = model.compute_accuracy(features, y_val, adj, val_mask)
@@ -29,7 +29,7 @@ def train_with_gd(model, features, adj, y_train, y_val, train_mask, val_mask, ea
         if early_stopping:
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
-                val_epoch = epoch 
+                val_epoch = epoch
             else:
                 if epoch - val_epoch == 10:
                     print("validation loss has not improved for 10 epochs... stopping early")
@@ -39,19 +39,21 @@ def train_with_gd(model, features, adj, y_train, y_val, train_mask, val_mask, ea
 
 def train(early_stopping, dropout, weight_decay, epochs):
     adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data('Cora')
-    
-    model = numpyGCN(input_dim=features.shape[1], hidden_dim=16, output_dim=y_train.shape[1])
-    train_with_gd(model, features, adj, y_train, y_val, train_mask, val_mask, 
-        early_stopping=early_stopping, lr=0.1, d=dropout, w_d=weight_decay, epochs=epochs)
 
-    test_loss = model.calc_loss(features, y_test, adj, test_mask, weight_decay)
+    model = numpyGCN(
+        input_dim=features.shape[1],
+        hidden_dim=16,
+        output_dim=y_train.shape[1],
+        dropout=dropout,
+        weight_decay = weight_decay
+    )
+    train_with_gd(model, features, adj, y_train, y_val, train_mask, val_mask,
+        early_stopping=early_stopping, lr=0.1, epochs=epochs)
+
+    test_loss = model.calc_loss(features, y_test, adj, test_mask)
     test_accuracy = model.compute_accuracy(features, y_test, adj, test_mask)
 
     print("test_loss=", "{:.5f}".format(test_loss), "test_acc=", "{:.5f}".format(test_accuracy))
 
 if __name__ == '__main__':
     train(early_stopping=True, dropout=0.2, weight_decay=0.0005, epochs=200)
-
-
-
-
