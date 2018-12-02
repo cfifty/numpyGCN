edc5f07b88e3ea7429e785e011fc3d56a3a1badc
Danny Qiu <dqiu55@gmail.com>
2018-12-02 17:45:54 -0500

    Fix testing script parameters

--- numpyGCN/testing.py
+++ numpyGCN/testing.py
@@ -27,8 +27,8 @@ def test_calc_loss():
 # test one gradient descent update
 def test_gd_step():
     adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data('Cora')
-    model = numpyGCN(input_dim=features.shape[1], hidden_dim=16, output_dim=y_train.shape[1])
-    model.gd_update(features, y_train, adj, train_mask, lr=0.1, d=0.2, w_d=0.0)
+    model = numpyGCN(input_dim=features.shape[1], hidden_dim=16, output_dim=y_train.shape[1], dropout=.2, weight_decay=0)
+    model.gd_update(features, y_train, adj, train_mask, lr=0.1)
 
 if __name__ == '__main__':
     test_gd_step()
