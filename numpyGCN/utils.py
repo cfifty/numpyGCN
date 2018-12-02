95db130f49e711b2252f3a2bbf64ffa36424975f
Danny Qiu <dqiu55@gmail.com>
2018-12-02 16:11:39 -0500

    Use csr instead of coo for adjacency matrix

--- numpyGCN/utils.py
+++ numpyGCN/utils.py
@@ -47,13 +47,13 @@ def normalize_features(mx):
 # coordinate list sparse matrix for normalized adjacency matrix
 def normalize_adj(adj):
     """Symmetrically normalize adjacency matrix."""
-    adj = sp.coo_matrix(adj)
+    adj = sp.csr_matrix(adj)
     adj += sp.eye(adj.shape[0])
     rowsum = np.array(adj.sum(axis=1))
     d_inv_sqrt = np.power(rowsum, -0.5).flatten()
     d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
     d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
-    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocsr()
 
 def load_data(dataset_str):
     """
